d875cff0fd78953a1c32e73ffb4ac6f8f5d0fa03
GG <1091308222@qq.com>
2025-09-11 00:51:51 +0800

    Initial commit: add all project files

--- LessonCS/LessonCS/Program.cs
+++ LessonCS/LessonCS/Program.cs
@@ -12,46 +12,128 @@
 
 
 using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Resources;
 
 namespace LessonCS
 {
-    enum E_FirstLesson
+    enum E_Job
     {
-        zhang,
-        han,
-        wen
-    }    enum E_Monster
-    {
-        Boss,
-        Normal
-    }    enum E_Player
-    {
-        MainPlayer,
-        NPC
+        None,
+        [Description("战士")]
+        Warrior,
+        [Description("法师")]
+        Mage,
+        [Description("射手")]
+        Archer,
+        [Description("奶妈")]
+        Priest
     }
     internal class Program
     {
-        
-        static void Main(string[] args)
+
+
+        public static void ShowSkills(E_Job job)
         {
-            E_Player playerType = E_Player.MainPlayer;
-            Console.WriteLine("玩家类型："+playerType);
-            //E_Monster MonsterType = E_Monster.Boss;
-            String MonsterType = Console.ReadLine();
-            
-            switch (MonsterType)
-            {   
-                case E_Monster.Boss:
-                    Console.WriteLine("Boss类型："+MonsterType);
+            string temp = GetDesc(job);
+           // Console.WriteLine("当前职业是：");
+           Console.WriteLine("你的职业是："+temp);
+            switch (job)
+            {
+                case E_Job.Warrior:
+                    
+                    Console.WriteLine("技能是："+"砍"); 
+
                     break;
-                case E_Monster.Normal:
-                    Console.WriteLine("Boss类型："+MonsterType);
+                case E_Job.Mage:
+                    Console.WriteLine("技能是："+"火球"); 
+    
+                    break;
+                case E_Job.Archer:
+                    Console.WriteLine("技能是："+"射"); 
+
+                    break;
+                case E_Job.Priest:
+                    Console.WriteLine("技能是："+"加血"); 
+
                     break;
                 default:
-                    Console.WriteLine("XXXxxx");
+                    Console.WriteLine("错误");
+
                     break;
             }
         }
-        
+        public static string GetDesc(E_Job job)
+        {
+            FieldInfo Info = job.GetType().GetField(job.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])Info.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            else
+            {
+                return job.ToString();
+            }
+            
+            
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入职业：");
+                String Player = Console.ReadLine();
+                
+                if (Player == "0")
+                {
+                    break;
+                }
+                else
+                {
+                    Enum.TryParse<E_Job>(Player, true, out E_Job job);
+                    ShowSkills(job);
+                }
+                
+            }
+        }
+
+
+        //     E_Player playerType = E_Player.MainPlayer;
+        //     Console.WriteLine("玩家类型："+playerType);
+        //     //E_Monster MonsterType = E_Monster.Boss;
+        //     String MonsterType = Console.ReadLine();
+        //     String Boss = E_Monster.Boss.ToString();
+        //     String Normal = E_Monster.Normal.ToString();
+        //     int S1;
+        //     if (MonsterType == Boss)
+        //     {
+        //         Console.WriteLine(Boss);
+        //         S1 = 1;
+        //     }
+        //     else if (MonsterType == Normal)
+        //     {
+        //         Console.WriteLine(Normal);
+        //         S1 = 2;
+        //     }
+        //     else
+        //     {
+        //         S1 = 3;
+        //     }
+        //     switch (S1)
+        //     {   
+        //         case 1:
+        //             Console.WriteLine("Boss类型："+MonsterType);
+        //             break;
+        //         case 2:
+        //             Console.WriteLine("Boss类型："+MonsterType);
+        //             break;
+        //         default:
+        //             Console.WriteLine("XXXxxx");
+        //             break;
+        //     }
+        // }
+        //
     }
 }
